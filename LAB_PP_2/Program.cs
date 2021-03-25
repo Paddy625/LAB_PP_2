f1d9abd286509ba4eb9fe3f3e89a6834ca0f54d4
Adam Twardowski <adamtwardowski1@interia.pl>
2021-03-25 22:03:38 +0100

    Dodaj pliki projektów.

---
+++ LAB_PP_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LAB_PP_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(@"students.json");
+            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(json);
+
+            foreach (var s in students)
+            {
+                Console.WriteLine(s.studentId + ":\t" + s.studentName);
+            }
+        }
+
+    }
+
+    class Student
+    { 
+        public int studentId { set; get; }
+        public string studentName { set; get; }
+    }
+}
